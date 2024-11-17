ce2bd671f86865a51771e4962b61036c3d95de5f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2024-11-18 02:37:32 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <div>
+          <h3>e-shop</h3>
+          <p>
+            Your one-step for all your needs.Shop with use and experience the
+            best online shopping experiences
+          </p>
+        </div>
+        <div>
+          <h4>Quick links</h4>
+          <ul>
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>Shop</Link>
+            </li>
+            <li>
+              <Link>About</Link>
+            </li>
+            <li>
+              <Link>Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Follow us</h4>
+          <div>
+            <a href="">
+              <FaFacebook />
+            </a>
+            <a href="">
+              <FaTwitter />
+            </a>
+            <a href="">
+              <FaGithub />
+            </a>
+            <a href="">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
