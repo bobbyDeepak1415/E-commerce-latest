1bb02a7c6812154c7f4b44efb4dbe6acd8688f4e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2024-11-18 15:29:45 +0530

    i changed the title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer>
-      <div>
+    <footer className="bg-gray-800 text-white py-8 px-4 md:px-16 lg:px-24">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         <div>
-          <h3>e-shop</h3>
-          <p>
+          <h3 className="text-xl font-semibold">e-shop</h3>
+          <p className="mt-4">
             Your one-step for all your needs.Shop with use and experience the
             best online shopping experiences
           </p>
         </div>
-        <div>
-          <h4>Quick links</h4>
-          <ul>
+        <div className="flex flex-col md:items-center">
+          <h4 className="text-lg font-semibold">Quick links</h4>
+          <ul className="mt-4 space-y-2">
             <li>
-              <Link>Home</Link>
+              <Link to="/" className="hover:underline">
+                Home
+              </Link>
             </li>
             <li>
-              <Link>Shop</Link>
+              <Link to="/shop" className="hover:underline">
+                Shop
+              </Link>
             </li>
             <li>
-              <Link>About</Link>
+              <Link to="/about" className="hover:underline">
+                About
+              </Link>
             </li>
             <li>
-              <Link>Contact</Link>
+              <Link to="/contact" className="hover:underline">
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
@@ -37,7 +46,7 @@ function Footer() {
               <FaFacebook />
             </a>
             <a href="">
-              <FaTwitter className="text-blue-500"/>
+              <FaTwitter className="text-blue-500" />
             </a>
             <a href="">
               <FaGithub />
@@ -46,6 +55,10 @@ function Footer() {
               <FaLinkedin />
             </a>
           </div>
+          <form>
+            <input type="email" />
+            <button>Subscribe</button>
+          </form>
         </div>
       </div>
     </footer>
